c1e4ff5f1c7b3e4c445f9901db447efa3b9e43a3
Maiquera <maicolmenezes@gmail.com>
2024-09-21 11:40:56 -0300

    new about sectiuon

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-function toggle(){
-    var header = document.getElementById("navbar")
-    header.classList.toggle('active')
-}
 
-function translate(evt, language) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    langchoise = document.getElementsByClassName("langchoise");
-    for (i = 0; i < tablinks.length; i++) {
-        langchoise[i].className = langchoise[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  
-  document.getElementById("defaultOpen").click();
+
